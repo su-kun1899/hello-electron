eab2729d53499857d70522d119f54d269263febe
su-kun1899 <higedrum.coz@gmail.com>
2017-07-06 20:21:00 +0900

    Implements ready event function

--- index.js
+++ index.js
@@ -5,7 +5,19 @@
 
 const electron = require('electron');
 const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
 
 app.on('ready', function(){
-    // clear window
+    // create window
+    mainWindow = new BrowserWindow({width: 600, height: 400});
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    
+    // TODO For debug
+    mainWindow.webContents.openDevTools();
+    
+    mainWindow.on('closed', function(){
+        mainWindow = null;
+    });
 });
