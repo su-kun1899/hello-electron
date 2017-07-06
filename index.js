8477c16607a023b92d45cc868652a8bd68757b6e
su-kun1899 <higedrum.coz@gmail.com>
2017-07-06 20:13:48 +0900

    Add index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// index.js (main process)
+// - GUI (renderer process)
+
+const electron = require('electron');
+const app = electron.app;
+
+app.on('ready', function(){
+    // clear window
+});
