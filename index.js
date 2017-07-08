4a00111f61b4a6e702701440e407595241ca9979
su-kun1899 <higedrum.coz@gmail.com>
2017-07-08 18:46:40 +0900

    Customize menu

--- index.js
+++ index.js
@@ -6,10 +6,25 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 
 let mainWindow;
 
+let menuTemplate = [{
+    label: 'Hello Electron',
+    submenu: [
+        { label: 'About' },
+        { type: 'separator' },
+        { label: 'Settings' },
+        { type: 'separator' },
+        { label: 'Quit' }
+    ]
+}];
+
+let menu = Menu.buildFromTemplate(menuTemplate);
+
 function createMainWindow() {
+    Menu.setApplicationMenu(menu);
     // create window
     mainWindow = new BrowserWindow({ width: 600, height: 400 });
     mainWindow.loadURL('file://' + __dirname + '/index.html');
