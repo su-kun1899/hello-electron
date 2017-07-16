6c5b9ad10a3a32cb5be33e3a11834cc8777e344d
su-kun1899 <higedrum.coz@gmail.com>
2017-07-16 13:45:11 +0900

    Check default color

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const ipcMain = electron.ipcMain;
 
 let mainWindow;
 let settingsWindow;
+let backgroundColor = 'skyblue';
 
 let menuTemplate = [{
     label: 'Hello Electron',
@@ -28,6 +29,10 @@ let menu = Menu.buildFromTemplate(menuTemplate);
 
 ipcMain.on('settings_changed', function (event, color) {
     mainWindow.webContents.send('set_bgcolor', color);
+});
+
+ipcMain.on('get_bgcolor', function (event) {
+    event.returnValue = backgroundColor;
 });
 
 function createMainWindow() {
